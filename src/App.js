22a45bab8c648bf20c8bb14b264f0d20e7a222ed
sadaramdeepthi <sadaramdeepthi@gmail.com>
2021-03-09 16:07:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import images from "./images/images";
+import NetworkDetector from "./components/NetworkDetector";
 
 function App() {
+  const renderImage = () => {
+    return (
+      <div className="image-list">
+        {images.map((data) => (
+          <img src={data.image} alt="random" key={data.id} className="image" />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="page-title">The Image Gallery</p>
+      {renderImage()}
     </div>
   );
 }
 
-export default App;
+export default NetworkDetector(App);
